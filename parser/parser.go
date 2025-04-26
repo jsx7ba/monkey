15a5524f647f80880047f1cfb0d232992f462674
jsx7ba <jscheiber@gmail.com>
2025-04-26 19:43:53 -0400

    support for grouped expressions

--- parser/parser.go
+++ parser/parser.go
@@ -59,6 +59,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 	p.registerPrefix(token.TRUE, p.parseBoolean)
 	p.registerPrefix(token.FALSE, p.parseBoolean)
+	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
 
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
@@ -153,6 +154,15 @@ func (p *Parser) parseLetStatement() (*ast.LetStatement, bool) {
 	}
 
 	return stmt, true
+}
+
+func (p *Parser) parseGroupedExpression() ast.Expression {
+	p.nextToken()
+	exp := p.parseExpression(LOWEST)
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+	return exp
 }
 
 func (p *Parser) parseExpressionStatement() (*ast.ExpressionStatement, bool) {
