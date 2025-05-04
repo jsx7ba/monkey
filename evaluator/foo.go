563f71b1d74bef34c496bf48513f1517d03fc0c5
jsx7ba <jscheiber@gmail.com>
2025-05-04 19:38:30 -0400

    renaming to deal with case sensitivity

---
+++ evaluator/foo.go
@@ -0,0 +1,41 @@
+package evaluator
+
+import (
+	"monkey/ast"
+	"monkey/object"
+)
+
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBoolObject(node.Value)
+	}
+	return nil
+}
+
+func nativeBoolToBoolObject(value bool) *object.Boolean {
+	if value {
+		return TRUE
+	}
+	return FALSE
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+	return result
+}
