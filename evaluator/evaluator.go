641dbd03fed2594b07c229e99a497c183118c429
jsx7ba <jscheiber@gmail.com>
2025-05-12 18:05:12 -0400

    add array and index expression evaluation

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -73,6 +73,22 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		return applyFunction(function, args)
 	case *ast.StringLiteral:
 		return &object.String{Value: n.Value}
+	case *ast.ArrayLiteral:
+		elements := evalExpressions(n.Elements, env)
+		if len(elements) == 1 && isError(elements[0]) {
+			return elements[0]
+		}
+		return &object.Array{Elements: elements}
+	case *ast.IndexExpression:
+		left := Eval(n.Left, env)
+		if isError(left) {
+			return left
+		}
+		index := Eval(n.Index, env)
+		if isError(index) {
+			return index
+		}
+		return evalIndexExpression(left, index)
 	}
 	return nil
 }
@@ -303,4 +319,24 @@ func unwrapReturnValue(obj object.Object) object.Object {
 		return returnValue.Value
 	}
 	return obj
+}
+
+func evalIndexExpression(left, index object.Object) object.Object {
+	switch {
+	case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
+		return evalArrayIndexExpression(left, index)
+	default:
+		return newError("index operator not supported: %s", left.Type())
+	}
+}
+
+func evalArrayIndexExpression(array, index object.Object) object.Object {
+	arrayObject := array.(*object.Array)
+	idx := index.(*object.Integer).Value
+	end := int64(len(arrayObject.Elements) - 1)
+
+	if idx < 0 || idx > end {
+		return NULL
+	}
+	return arrayObject.Elements[idx]
 }
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -313,3 +313,32 @@ func TestBuiltinFunction(t *testing.T) {
 		}
 	}
 }
+
+func TestArrayIndexExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{"[1, 2, 3][0]", 1},
+		{"[1, 2, 3][1]", 2},
+		{"[1, 2, 3][2]", 3},
+		{"let i = 0; [1][i];", 1},
+		{"[1, 2, 3][1 + 1];", 3},
+		{"let myArray = [1, 2, 3]; myArray[2];", 3},
+		{"let myArray = [1, 2, 3]; myArray[0] + myArray[1] + myArray[2];", 6},
+		{"let myArray = [1, 2, 3]; let i = myArray[0]; myArray[i]", 2},
+		{"[1,2,3][3]", NULL},
+		{"[1,2,3][-1]", NULL},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		integer, ok := tt.expected.(int)
+		if ok {
+			testIntegerObject(tt.input, t, evaluated, int64(integer))
+		} else {
+			testNullObject(tt.input, t, evaluated)
+		}
+	}
+
+}
