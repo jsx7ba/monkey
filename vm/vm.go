b4cf029b9528b6190765c03a16da9f2de939e6f1
jsx7ba <jscheiber@gmail.com>
2025-08-13 20:38:47 -0400

    add parameter bindings for functions in the VM

--- vm/vm.go
+++ vm/vm.go
@@ -144,19 +144,32 @@ func (vm *VM) Run() error {
 			vm.sp = frame.basePointer - 1
 			err = vm.push(Null)
 		case code.OpCall:
-			fn, ok := vm.stack[vm.sp-1].(*object.CompiledFunction)
-			if !ok {
-				return fmt.Errorf("calling non-function")
+			numArgs := code.ReadUint8(ins[ip+1:])
+			vm.currentFrame().ip += 1
+			err = vm.callFunction(int(numArgs))
+			if err != nil {
+				return err
 			}
-			frame := NewFrame(fn, vm.sp)
-			vm.pushFrame(frame)
-			vm.sp = frame.basePointer + fn.NumLocals
 		}
 
 		if err != nil {
 			return err
 		}
 	}
+	return nil
+}
+
+func (vm *VM) callFunction(numArgs int) error {
+	fn, ok := vm.stack[vm.sp-1-numArgs].(*object.CompiledFunction)
+	if !ok {
+		return fmt.Errorf("calling non-function")
+	}
+	if numArgs != fn.NumParameters {
+		return fmt.Errorf("wrong number of arguments: want=%d, got=%d", fn.NumParameters, numArgs)
+	}
+	frame := NewFrame(fn, vm.sp-numArgs)
+	vm.pushFrame(frame)
+	vm.sp = frame.basePointer + fn.NumLocals
 	return nil
 }
 
